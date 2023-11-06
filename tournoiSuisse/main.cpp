423739ae2a550430253e730f472d3493eb36fd86
RIGAT-Pierre-andre-2326090par <145963275+RIGAT-Pierre-andre-2326090par@users.noreply.github.com>
2023-11-06 12:58:02 +0100

    Update main.cpp

--- tournoiSuisse/main.cpp
+++ tournoiSuisse/main.cpp
@@ -64,27 +64,26 @@ void tournoiSansBracketLooser(string typeTournoi, vector <int> team, unsigned nb
 void tournoiAvecLooserBracket(string typeTournoi, vector <int> team, unsigned nbTeam){
     if (estUnePuissanceDe2(nbTeam) == -1) cout << "refaite les équipes" << endl;
     else {
-        int round = 1;
         int combat = 1;
-        vector <int> team2
-        for (int j = 0 ; j < estUnePuissanceDe2(nbTeam) ; ++j){
-            cout << "round " << round << endl;
-            for (size_t i = 0 ; i < team.size() - 1; ++i){
-                cout << "combat " << combat << endl;
-                if (leJ1AGagne(/*team[0],team[1]*/)) {
-                    cout << team[0 + i] << " gagne" << endl;
-                    team.erase(team.begin() + i + 1);
-                }
-                else{
-                    cout << team[1 + i] << " gagne" << endl;
-                    team.erase(team.begin() + i);
-                }
-                ++combat;
-                afficheVector(team);
+        vector <int> team2;
+        for (size_t i = 0 ; i < team.size() - 1; ++i){
+            cout << "combat " << combat << endl;
+            if (leJ1AGagne(/*team[0],team[1]*/)) {
+                cout << team[0 + i] << " gagne" << endl;
+                team2.push_back(team[i + 1]);
+                team.erase(team.begin() + i + 1);
             }
-            ++round;
+            else{
+                cout << team[1 + i] << " gagne" << endl;
+                team2.push_back(team[i]);
+                team.erase(team.begin() + i);
+            }
+            ++combat;
+            afficheVector(team);
+            afficheVector(team2);
         }
-        cout << "le vainqueur " << typeTournoi << " est " << team[0] << endl;
+        tournoiSansBracketLooser("du winner bracket",team,nbTeam/2);
+        tournoiSansBracketLooser("du looser bracket",team2,nbTeam/2);
     }
 }
 
