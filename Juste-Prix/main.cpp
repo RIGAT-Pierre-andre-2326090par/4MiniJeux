b9e89e8c0dbaa95c96b886470d8d6f22cc8fc202
Alex Goncalves Rodrigues <144851148+GONCALVES-RODRIGUES-Alex-2326052@users.noreply.github.com>
2023-11-10 14:40:31 +0100

    Update main.cpp

--- Juste-Prix/main.cpp
+++ Juste-Prix/main.cpp
@@ -1,92 +1,77 @@
-#include <iostream>
+// JustePrix.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
 using namespace std;
+#include <iostream>
 
+void justePrixEtEquipe(const unsigned& seed) {
+	cout << "Essayer de trouver le juste prix." << endl;
+	cout << "Le but est ausi d'essayer de faire le moins d'essaie." << endl;
+	cout << "Bonne chance !" << endl;
+	int Equipe1, Equipe2;
+	int valeuratrouver;
+	srand(seed);
+	valeuratrouver = rand() % 50;
+	cout << "Qui sont les 2 equipes qui s'affronte ?" << endl;
+	cout << "1er equipe :" << endl;
+	cin >> Equipe1;
+	cout << "2nd equipe :" << endl;
+	cin >> Equipe2;
+	int cpt1 = 0;
+	int cpt2 = 0;
+	bool continuer1 = true, continuer2 = true;
+	while (continuer1 == true) {
+		cout << "A vous de jouer " << Equipe1 << endl;
+		int valeurtap;
+		cout << "Taper le chiffre :" << endl;
+		cin >> valeurtap;
+		if (valeuratrouver < valeurtap) {
+			cout << "C'est moins !" << endl;
+			valeurtap = 0;
+			cpt1 = cpt1 + 1;
+		}
+		else if (valeuratrouver > valeurtap) {
+			cout << "C'est plus !" << endl;
+			valeurtap = 0;
+			cpt1 = cpt1 + 1;
+		}
+		else if (valeuratrouver == valeurtap) {
+			cout << "OUAIS OUAIS OUAIS, Bravo tu a trouver le prix correcte." << endl;
+			cpt1 = cpt1 + 1;
+			cout << "Votre score d'essaie est de " << cpt1 << endl;
+			continuer1 = false;
+		}
+	}
+	while (continuer2 == true){
+		int valeurtap;
+		cout << "L'equipe a jouer est la " << Equipe2 << endl;
+		cout << "Taper le chiffre :" << endl;
+		cin >> valeurtap;
+		if (valeuratrouver < valeurtap) {
+			cout << "C'est moins !" << endl;
+			valeurtap = 0;
+			cpt2 = cpt2 + 1;
+		}
+		else if (valeuratrouver > valeurtap) {
+			cout << "C'est plus !" << endl;
+			valeurtap = 0;
+			cpt2 = cpt2 + 1;
+		}
+		else if (valeuratrouver == valeurtap) {
+			cpt2 = cpt2 + 1;
+			cout << "OUAIS OUAIS OUAIS, Bravo tu a trouver le prix correcte." << endl;
+			cout << "Votre score d'essaie est de " << cpt2 << endl;
+			continuer2 = false;
+		}
+	}
 
-void justePrixEtEquipe() {
-    cout << "Essayer de trouver le juste prix." << endl;
-    cout << "Le but est ausi d'essayer de faire le moins d'essaie." << endl;
-    cout << "Bonne chance !" << endl;
-    string Equipe1, Equipe2;
-    int valeuratrouver = 0;
-    cout << "Qui sont les 2 equipes qui s'affronte ?" << endl;
-    cout << "1er equipe :" << endl;
-    cin >> Equipe1;
-    cout << "2nd equipe :" << endl;
-    cin >> Equipe2;
-    string Equipe0;
-    cout << " Qui commence ?" << endl;
-    cin >> Equipe0;
-    int cpt1 = 0;
-    int cpt2 = 0;
-    bool continuer = true;
-    while (continuer == true) {
-        if (Equipe0 == Equipe1) {
-            cout << Equipe2 << " veuillez taper le nombre a trouver pour l'equipe adverse" << endl;
-            cout << "Entre 0 et 10000 svp" << endl;
-            cin >> valeuratrouver;
-            cout << "A vous de jouer " << Equipe1 << endl;
-            int valeurtap;
-            cout << "Taper le chiffre :" << endl;
-            cin >> valeurtap;
-            if (valeuratrouver < valeurtap) {
-                cout << "C'est moins !" << endl;
-                valeurtap = 0;
-                cpt1 = cpt1 + 1;
-            }
-            else if (valeuratrouver > valeurtap) {
-                cout << "C'est plus !" << endl;
-                valeurtap = 0;
-                cpt1 = cpt1 + 1;
-            }
-            else if (valeuratrouver == valeurtap) {
-                cpt1 = cpt1 + 1;
-                cout << "OUAIS OUAIS OUAIS, Bravo tu a trouver le prix correcte." << endl;
-                cout << "Votre score d'essaie est de " << cpt1 << endl;
-                cout << "Passer au tour de l'equipe suivante ? y/n" << endl;
-                string equipesuivante;
-                cin >> equipesuivante;
-                if (equipesuivante == "n")
-                {
-                    continuer = false;
-                }
-            }
-        }
-        if (Equipe0 == Equipe2) {
-            cout << Equipe1 << " veuillez taper le nombre a trouver pour l'equipe adverse" << endl;
-            cout << "Entre 0 et 10000 svp" << endl;
-            cin >> valeuratrouver;
-            cout << "A vous de jouer " << Equipe2 << endl;
-            int valeurtap;
-            cout << "Taper le chiffre :" << endl;
-            cin >> valeurtap;
-            if (valeuratrouver < valeurtap) {
-                cout << "C'est moins !" << endl;
-                valeurtap = 0;
-                cpt2 = cpt2 + 1;
-            }
-            else if (valeuratrouver > valeurtap) {
-                cout << "C'est plus !" << endl;
-                valeurtap = 0;
-                cpt2 = cpt2 + 1;
-            }
-            else if (valeuratrouver == valeurtap) {
-                cpt2 = cpt2 + 1;
-                cout << "OUAIS OUAIS OUAIS, Bravo tu a trouver le prix correcte." << endl;
-                cout << "Votre score d'essaie est de " << cpt2 << endl;
-                cout << "Passer au tour de l'equipe suivante ? y/n" << endl;
-                string equipesuivante;
-                cin >> equipesuivante;
-                if (equipesuivante == "n")
-                {
-                    continuer = false;
-                }
-            }
-        }
-    }
-    if (cpt1 < cpt2) {
-        cout << "C'est l'equipe " << Equipe2 << "qui a gagner avec " << cpt2 << "essaie !" << endl;
-    }
-    else if (cpt2 < cpt1) {
-        cout << "C'est l'equipe " << Equipe1 << "qui a gagner avec " << cpt1 << "essaie !" << endl;
-    }
+	if (cpt1 < cpt2) {
+		cout << "C'est l'equipe " << Equipe1 << " qui a gagner avec " << cpt1 << " essaie comparer a " << cpt2 <<" essaie pour l'equipe " << Equipe2 << endl;
+	}
+	else if (cpt2 < cpt1) {
+		cout << "C'est l'equipe " << Equipe2 << " qui a gagner avec " << cpt2 << " essaie comparer a " << cpt1 <<" esaaie pour l'equipe " << Equipe1 << endl;
+	}
+}
+int main()
+{
+	justePrixEtEquipe(1);
 }
